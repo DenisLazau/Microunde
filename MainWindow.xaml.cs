e119906888dbc954ac617924e95e337e3b977dd1
denis <denis.lazau@hotmail.com>
2024-12-10 08:23:38 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Microunde
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int ticker = 0;
+        public MainWindow()
+        {
+            Microunde microunde = new Microunde();
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+            StareUsa.Text = "Usa Inchisa";
+            StareMicrounde.Text = "Oprit";
+            ticker = 0;
+            Ticker.Text = ticker.ToString();
+        }
+
+        private void InchideUsa(object sender, RoutedEventArgs e)
+        {
+            StareUsa.Text = "Usa Inchisa";
+        }
+
+        private void DeschideUsa(object sender, RoutedEventArgs e)
+        {
+            StareUsa.Text = "Usa Deschisa";
+            StareMicrounde.Text = "Oprit";
+            Ticker.Text = ticker.ToString();
+        }
+
+        private void Pornit(object sender, RoutedEventArgs e)
+        {
+            if(StareUsa.Text == "Usa Inchisa")
+            {
+                StareMicrounde.Text = "Pornit";
+                ticker = 30;
+            }
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if(ticker > 0)
+            {
+                ticker--;
+                Ticker.Text = ticker.ToString();
+            }
+        }
+    }
+}
---
+++ Microunde.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microunde
+{
+    public class Microunde
+    {
+        public Enums.StareUsa StareUsa
+        { get; set; }
+
+        public Enums.StareMicrounde StareMicrounde
+        { get; set; }
+
+        public int Ticker
+        { get; set; }
+
+        public Microunde()
+        {
+            StareUsa = Enums.StareUsa.Inchisa;
+            StareMicrounde = Enums.StareMicrounde.Oprit;
+            Ticker = 0;
+        }
+
+        public void InchideUsa()
+        {
+            StareUsa = Enums.StareUsa.Inchisa;
+        }
+
+        public void DeschideUsa()
+        {
+            StareUsa = Enums.StareUsa.Deschisa;
+            StareMicrounde = Enums.StareMicrounde.Oprit;
+            Ticker = 0;
+        }
+
+        public void Pornit()
+        {
+            if (StareUsa == Enums.StareUsa.Inchisa)
+            {
+                StareMicrounde = Enums.StareMicrounde.Pornit;
+            }
+        }
+    }
+}
